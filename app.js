7b7605dfdaa6393be81011943ac59327935d5c98
Babita <babitabisht2018@gmail.com>
2019-04-24 09:37:12 +0530

    code improved

--- app.js
+++ app.js
@@ -22,51 +22,156 @@ var mailer = nodemailer.createTransport({
 })
 
 
-
-app.post('/sendMail' , (req,res)=>{
-
-
-  console.log('req.body' , req.body);
-console.log(req.body.email_type)
-
-
 mailer.use('compile' , hbs({
-  // partialsDir: 'views/header',
+
   viewEngine: {
     extName: '.hbs',
     partialsDir: 'views/header',
   },
-  viewPath : 'views/'+req.body.email_type ,
+  viewPath : 'views/' ,
   extname  : '.hbs'
 }))
+
+
+// route or endpoint to send mail
+
+app.post('/sendMail' , (req,res)=>{
+
+console.log('req.body' , req.body);
+console.log(req.body.email_type)
+console.log(req.body.password)
+
+
+// checking the number of params for Invitation_Email
+if( req.body.email_type ==='Invitation_Email' ){
+
+console.log(req.body.length)
+
+let count=Object.keys(req.body).length;
+if(count < 4){
+  res.send({
+    "success" :false ,
+    "message" :"Insufficient information"
+  })
+}else{
+  sendMail(req ,res )
+}
+
+
+}
+
+// checking the number of params for Welcome_Email
+else if( req.body.email_type ==='Welcome_Email' ){
+
+console.log(req.body.length)
+
+let count=Object.keys(req.body).length;
+if(count < 6){
+  res.send({
+    "success" :false ,
+    "message" :"Insufficient information"
+  })
+}
+else{
+  sendMail(req ,res )
+}
+
+
+}
+
+// checking the number of params for Confirmation_Email
+else if( req.body.email_type ==='Confirmation_Email' ){
+
+console.log(req.body.length)
+
+let count=Object.keys(req.body).length;
+if(count < 3){
+  res.send({
+    "success" :false ,
+    "message" :"Insufficient information"
+  })
+}else{
+  sendMail(req ,res )
+}
+
+
+}
+
+// checking the number of params for ResetPassword_Email
+else if( req.body.email_type ==='ResetPassword_Email' ){
+
+console.log(req.body.length)
+
+let count=Object.keys(req.body).length;
+if(count < 5){
+  res.send({
+    "success" :false ,
+    "message" :"Insufficient information"
+  })
+}else{
+  sendMail(req ,res )
+}
+
+
+}
+
+// checking the number of params for Rejection_Email
+else if( req.body.email_type ==='Rejection_Email' ){
+
+console.log(req.body.length)
+
+let count=Object.keys(req.body).length;
+if(count < 4){
+  res.send({
+    "success" :false ,
+    "message" :"Insufficient information"
+  })
+}else{
+  sendMail(req ,res )
+}
+
+
+}
+
+
+
+
+
+
+} )
+
+
+ // function to send mail
+sendMail =(req,res)=>{
+
 
   mailer.sendMail( {
     from: 'Admin name <invitation-do-not-reply@interwork.com> ',
     to: 'babita@interwork.biz',
     subject: req.body.subject,
-    template: 'email',
+    template: req.body.email_type + '/email',
     context: {
-      superHeading:req.body.superHeading ,
-      message:req.body.message,
-      subHeading:req.body.subHeading ,
-      footerNote:req.body.footerNote ,
-      link:req.body.link
+      brandname:req.body.brandname,
+      link:req.body.link,
+      username:req.body.username,
+      password:req.body.password ,
+      admin_name:req.body.admin_name
 
     }
  },(err, response)=>{
 
   if(err){
     console.log(err)
-    res.send('bad email')
+    res.send('Email not sent !')
   }else{
-    res.send("good email")
+    res.send("Mail sent !")
   }
  })
 
-} )
+}
 
 
-
+// app running on port 3000
 app.listen('3000' ,()=>{
   console.log('app running successfully on port 3000')
 })
---
+++ readMe.md
@@ -0,0 +1,69 @@
+npm packages used :-
+a) nodemailer
+b) nodemailer-express-handlebars
+c) bodyParser
+d) express
+
+
+ All the templates are rendered by handlebars template engine  (https://handlebarsjs.com/)
+
+
+Total number of Email templates -  5 (Five)
+The following are the name of templates :-
+ a) Confirmation_Email
+ b) Invitation_Email
+ c) Rejection_Email
+ d) ResetPassword_Email
+ e) Welcome_Email
+
+
+The application is running on port 3000
+
+Name of route or endpoint -  /sendMail (http://localhost:3000/sendMail)
+
+The body of the endpoint (/sendMail) :-
+
+a) In case of  Welcome Email
+{
+	"email_type" :"Welcome_Email",
+ 	"subject" :"Subject of Welcome Mail" ,
+    "username":"jim@123",
+    "password" :'1234agdf'
+    "brandname" : "BrandABC" ,
+    "link"      : "interwork.biz"
+
+  }
+
+b) In case of Invitation Email
+
+ {	"email_type" :"Invitation_Email",
+ 	"subject" :"subject of Invitation Email" ,
+ 	"admin_name":"admin(name)",
+	"brandname" : "BrandABC" ,
+	"link":"interwork.biz"
+ }
+
+c) In case of Confirmation Email
+
+ {	"email_type" :"Confirmation_Email",
+ 	"subject" :" subject of confirmation Email" ,
+ 	"brandname" : "BrandABC"
+ }
+
+d) In case of  ResetPassword Email
+
+ {	"email_type" :"ResetPassword_Email",
+ 	"subject" :"subject of  ResetPassword Email" ,
+ 	"link": "interwork.biz",
+ 	"username" :"username" ,
+	"brandname" : "BrandABC"
+ }
+
+e) In case of Rejection Email
+
+ {	"email_type" :"Rejection_Email",
+ 	"subject" :" subject of Rejection Email" ,
+	"username" :"username" ,
+	"brandname" : "BrandABC"
+ }
+
